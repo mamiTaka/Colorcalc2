a487286a55848420f94364407161affab14a147d
高橋真未 <takahashimami@takahashimami-no-macbook-air.local>
2017-01-07 21:11:00 +0900

    Add HRColorPickerのPodsを導入
    カラーピッカー機能具備のためPodsを導入

---
+++ colorcalc2/Header.h
@@ -0,0 +1,14 @@
+//
+//  Header.h
+//  colorcalc2
+//
+//  Created by 高橋真未 on 2017/01/07.
+//  Copyright © 2017年 高橋真未. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+#import "HRColorPickerView.h"
+
+
+#endif /* Header_h */
--- colorcalc2/ViewController.swift
+++ colorcalc2/ViewController.swift
@@ -14,12 +14,19 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    
+    let view = HRColorPickerView(frame: self.view.bounds)
+    view.color = UIColor.greenColor()
+    view.addTarget(self,
+    action: #selector(ViewController.colorChanged(_:)),
+    forControlEvents: .ValueChanged)
+    self.view.addSubview(view)
     }
 
-
+    func colorChanged(sender: HRColorPickerView){
+    }
 }
 
