44daa8a542f0eb8263f5cdd2c05f9486d9e048f0
高橋真未 <takahashimami@takahashimami-no-macbook-air.local>
2017-01-09 00:25:03 +0900

    Add ColorPickerのPodFileを修正
    ターミナルからPod fileを修正/ヘッダーの名称を修正/カラーピッカー呼び出しの場所変更/swiftコードを2から3に修正

--- colorcalc2/ViewController.swift
+++ colorcalc2/ViewController.swift
@@ -13,17 +13,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        let view = HRColorPickerView(frame: self.view.bounds)
+        view.color = UIColor.green
+        view.addTarget(self,
+                       action: #selector(ViewController.colorChanged(sender:)),
+                       for: .valueChanged)
+        self.view.addSubview(view)
+        
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    
-    let view = HRColorPickerView(frame: self.view.bounds)
-    view.color = UIColor.greenColor()
-    view.addTarget(self,
-    action: #selector(ViewController.colorChanged(_:)),
-    forControlEvents: .ValueChanged)
-    self.view.addSubview(view)
+        
+        //swift2系の書き方。↑は3系に修正済みのもの。
+        //ここはメモリが足りなくなった時にしか呼ばれない。動いても真っ白画面で終わったのはそのため。
+//    let view = HRColorPickerView(frame: self.view.bounds)
+//    view.color = UIColor.greenColor()
+//    view.addTarget(self,
+//    action: #selector(ViewController.colorChanged(_:)),
+//    forControlEvents: .ValueChanged)
+//    self.view.addSubview(view)
     }
 
     func colorChanged(sender: HRColorPickerView){
---
+++ colorcalc2/colorcalc2-colorPicker.h
@@ -0,0 +1,13 @@
+//
+//  colorcalc2-colorPicker.h
+//  colorcalc2
+//
+//  Created by 高橋真未 on 2017/01/08.
+//  Copyright © 2017年 高橋真未. All rights reserved.
+//
+
+#ifndef colorcalc2_colorPicker_h
+#define colorcalc2_colorPicker_h
+//#import "HRColorPickerView.h"
+
+#endif /* colorcalc2_colorPicker_h */
