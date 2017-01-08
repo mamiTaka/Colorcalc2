76506939269c2696ffc1b1e87603a87046a3c4ba
高橋真未 <takahashimami@takahashimami-no-macbook-air.local>
2017-01-09 00:32:31 +0900

    Fix Podfileの修正
    ターミナルからPodfileを修正/ヘッダー名の修正/カラーピッカーの呼び出しの場所を変更/swift２から3のコードに修正

---
+++ Pods/Target Support Files/Pods-colorcalc2-colorcalc2Tests/Pods-colorcalc2-colorcalc2Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_colorcalc2_colorcalc2Tests : NSObject
+@end
+@implementation PodsDummy_Pods_colorcalc2_colorcalc2Tests
+@end
---
+++ Pods/Target Support Files/Pods-colorcalc2-colorcalc2Tests/Pods-colorcalc2-colorcalc2Tests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_colorcalc2_colorcalc2TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_colorcalc2_colorcalc2TestsVersionString[];
+
