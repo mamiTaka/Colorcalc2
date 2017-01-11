04a0c98c7cbb03f7a89eee3e55d81c208f552ba8
高橋真未 <takahashimami@takahashimami-no-macbook-air.local>
2017-01-12 00:15:09 +0900

    Add インターフェースの変更　Fix　ヘッダーのパス
    インタフェースのイメージを追加。ヘッダーのパスを設定追加。ただしエラーは修正しきれていない。

--- colorcalc2/colorcalc2-colorPicker.h
+++ colorcalc2/colorcalc2-colorPicker.h
@@ -1,13 +1,14 @@
 //
-//  colorcalc2-colorPicker.h
+//  colorcalc2-colorPicker.h.h
 //  colorcalc2
 //
-//  Created by 高橋真未 on 2017/01/08.
+//  Created by 高橋真未 on 2017/01/09.
 //  Copyright © 2017年 高橋真未. All rights reserved.
 //
 
-#ifndef colorcalc2_colorPicker_h
-#define colorcalc2_colorPicker_h
-//#import "HRColorPickerView.h"
+#ifndef colorcalc2_colorPicker_h_h
+#define colorcalc2_colorPicker_h_h
+#import "HRColorPickerView.h"
 
-#endif /* colorcalc2_colorPicker_h */
+
+#endif /* colorcalc2_colorPicker_h_h */
